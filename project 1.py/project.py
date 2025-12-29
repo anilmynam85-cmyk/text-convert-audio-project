40ca4825682cf8081cc0d12a673594d7f8842afb
anilmynam85-cmyk <anilmynam85@gmail.com>
2025-12-29 23:27:27 +0530

    Update project.py
    add text

--- project 1.py/project.py
+++ project 1.py/project.py
@@ -2,7 +2,7 @@ from gtts import gTTS
 from playsound import playsound
 audio="speech.mp3"
 langvage="en"
-sp=gTTS(text=" ",lang=langvage,slow=False)
+sp=gTTS(text="Hello python  ",lang=langvage,slow=False)
 sp.save(audio)
 playsound(audio)
 print("=======audio is playing=====")
